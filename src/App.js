a39f8fa9d14589440ebc4e5d8c82297694228d54
Rushda Ansari <iamrushdaa@gmail.com>
2023-09-11 14:15:05 +0530

    Created ui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,100 @@
 
 function App() {
   return (
-    <div className="App">
-        <h1>
-          Hello world.
-        </h1>
+    <div className="container">
+      <nav className="navbar app"> App bar</nav>
+      <nav className="navbar board"> Board bar</nav>
+      <div className="board-columns">
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Third</li>
+            <li>Third</li>
+            <li>Third</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Third</li>
+            <li>Third</li>
+            <li>Third</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div class="column">
+          <header>Brainstrom</header>
+          <ul>
+            <li>
+              <img src='./img-design.png'/>
+              Design & Research
+              </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+
+
+
+
+
+        
+      </div>
     </div>
   );
 }
